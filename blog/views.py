ded0d2ff4484fde621cb21022917a5aaf7b75960
mohammadgholampour <m.gholampour2002@gmail.com>
2024-08-19 04:39:32 -1200

    Git Project

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+# blog/views.py
+
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
